b0502349551cac2a05f83d6a5e58fe091238f2ae
Gabriel da Rosa Silveira <140091@upf.br>
2021-02-18 22:45:57 -0300

    updated regex of input to receive arguments and bug fix

--- server.py
+++ server.py
@@ -3,14 +3,15 @@ import threading
 from connection import Connection
 import re
 
+
 HOST, PORT = '127.0.0.1', 8888
 
-job = []
+job = []  # [command, args]
 connections = []
 
 
 def manager():
-    """ method responsible for manage user input and run accept_connections thread """
+    """ method responsible for run accept_connections thread, manage user input and execute local methods """
 
     threading.Thread(target=accept_connections,).start()
 
@@ -20,6 +21,11 @@ def manager():
         if job:
             if job[0] == 'list':
                 list_connections()
+            elif job[0] == 'exit':
+                stop_threads()
+                break
+            elif job[0] == 'spy':
+                enable_remote_viewing()
 
 
 def accept_connections():
@@ -37,13 +43,13 @@ def accept_connections():
 
 
 def convert_input_to_job(input_string):
-    """ validates input with regex and converts it to job """
+    """ validates input command with regex and converts it to job """
     global job
 
-    job_command = re.search(r'^([a-z]+)$', input_string)
+    job_command = re.search(r'^([a-z]+)\s?(.*)$', input_string)
     if job_command:
-        if job_command.group(0) in ['list', ]:
-            job = ['list', '']
+        if job_command.group(0) in ['list', 'exit', 'spy']:
+            job = [job_command.group(0), job_command.group(0).strip()]
         else:
             print('Invalid command')
     else:
@@ -52,18 +58,28 @@ def convert_input_to_job(input_string):
 
 def list_connections():
     """ print all connections and reset job """
-    global job
 
-    print('\nID   ADDRESS')
-    for connection in connections:
-        print('{}   {}:{}'.format(str(connections.index(connection)).zfill(2),
-                                  connection.address[0],
-                                  str(connection.address[1])
-                                  ))
+    if len(connections) > 0:
+        print('\nID   ADDRESS')
+        for connection in connections:
+            print('{}   {}:{}'.format(str(connections.index(connection)).zfill(2),
+                                      connection.address[0],
+                                      str(connection.address[1])
+                                      ))
+        print('\n')
+    else:
+        print('No connections')
 
-    job = []
+    job.clear()
+
+
+def stop_threads():
+    pass
+
+
+def enable_remote_viewing():
+    pass
 
 
 if __name__ == '__main__':
     manager()
-
